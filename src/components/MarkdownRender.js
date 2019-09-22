21f9f593cbfa88513b406b19acc2354208c23d01
Gennadiy <genaydzhan70@gmail.com>
2019-09-22 20:26:53 +0600

    Add code highlighting

--- src/components/MarkdownRender.js
+++ src/components/MarkdownRender.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import RemarkMathPlugin from 'remark-math';
 import 'katex/dist/katex.min.css';
+import 'highlight.js/styles/default.css';
 import { InlineMath, BlockMath } from 'react-katex';
+import Highlight from 'react-highlight';
 
 export const MarkdownRender = (props) => {
   const newProps = {
@@ -14,6 +16,7 @@ export const MarkdownRender = (props) => {
       ...props.renderers,
       math: ({ value }) => <BlockMath>{ value }</BlockMath>,
       inlineMath: ({ value }) => <InlineMath>{ value }</InlineMath>,
+      code: ({ value }) => <Highlight>{ value }</Highlight>
     }
   };
   return (
